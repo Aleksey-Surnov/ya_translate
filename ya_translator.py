ccb3779e6b8a88913dbf336be47f59f23259c78c
Aleksey-Surnov <55957778+Aleksey-Surnov@users.noreply.github.com>
2020-03-02 18:42:00 +0300

    Поправка описания функций.

--- ya_translator.py
+++ ya_translator.py
@@ -2,21 +2,21 @@ from translator import Translator
 from colorama import init, Fore, Back
 init(convert=True)
 
-#trnsl.1.1.20200218T134412Z.9fee15ecfd8c15fb.eeb9a4e319a759ff23d82b35b26bd7923449a113
-"""Программа выполняет перевод слов с русского языка на английский"""
-"""Скрипт не чувствителен к регистру"""
+
+"""Программа выполняет перевод слов с русского языка на английский."""
+"""Скрипт не чувствителен к регистру."""
 
 ts = Translator(word='')
-"""Определим переменную как экземпляр класса"""
+"""Определим переменную как экземпляр класса."""
 
 def get_tarnslate_word(word=''):
-    """Выполнение перевода слова с русского на английский"""
+    """Выполнение перевода слова с русского языка на английский."""
     word_trans=ts.translate_me(word)
     return word_trans.lower()
 
 
 def input_word():
-    """Выполнение ввода слов"""
+    """Выполнение ввода слов."""
     while True:
         print(Fore.BLUE+ 'Введите слово для перевода или введите "stop" для остановки и получения результата: ', end='')
         ts.word=input().lower()
@@ -27,7 +27,7 @@ def input_word():
 
 
 def run_translate(word_list=[]):
-    """Переводчик"""
+    """Переводчик."""
     result_translate=[]
     for word in word_list:
         result_translate.append(get_tarnslate_word(word))
@@ -38,9 +38,9 @@ if __name__=="__main__":
 
     word_list=(input_word())
     translate_word=run_translate(word_list)
-    """Запуск переводчика"""
+    """Запуск переводчика."""
     for i in range(len(translate_word)):
-        """Выводим результат в цвете"""
+        """Выводим результат в цвете."""
         print(Fore.GREEN+translate_word[i])
 
 
