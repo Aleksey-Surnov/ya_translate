95f290dcc05d94a0a4e4b5e44ea243f6980415ed
Aleksey-Surnov <55957778+Aleksey-Surnov@users.noreply.github.com>
2020-03-02 18:36:45 +0300

    Поправка описания.

--- test_ya_translator.py
+++ test_ya_translator.py
@@ -3,35 +3,35 @@ from ya_translator import Translator, ts, run_translate
 
 
 class TestNameTranslate(unittest.TestCase):
-    """Тест для класса Translator"""
+    """Тест для класса Translator."""
 
     def setUp(self):
         self.ts = Translator(word='')
 
 
     def test_store_memory(self):
-        """Проверка сохранения слова или фразы в списке"""
+        """Проверка сохранения слова или фразы в списке."""
         self.ts.store_memory('привет мой друг')
         self.assertIn('привет мой друг', self.ts.memory)
 
 
     def test_correct_translate(self):
-        """Проверка правильности перевода простых фраз и слов"""
+        """Проверка правильности перевода простых фраз и слов."""
         format_trans=self.ts.translate_me("привет мой друг")
         self.assertEqual(format_trans.lower(), 'hi my friend')
 
 
 class TestNameProgramm(unittest.TestCase):
-    """Тест для программы ya_translator"""
+    """Тест для программы ya_translator."""
 
     def test_run_translate(self):
-        """Проверка правильности перевода и вывода списка слов"""
+        """Проверка правильности перевода и вывода списка слов."""
         list_trans = run_translate(['привет мой друг', 'художник', 'женщина'])
         self.assertEqual(list_trans, ['hi my friend', 'artist', 'woman'])
 
 
 if __name__=='__main__':
 
-    unittest.main()             # запуск теста
+    unittest.main()             # запуск теста.
 
 
